9d60febbabff1e177b8729154262c590af212ce9
anorazhu <zhusm@bc.edu>
2024-09-30 17:15:33 -0400

    Initial Commit

---
+++ WeatherAdviceTests/WeatherAdviceTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAdviceTests.swift
+//  WeatherAdviceTests
+//
+//  Created by Anora Zhu on 9/30/24.
+//
+
+import Testing
+@testable import WeatherAdvice
+
+struct WeatherAdviceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
